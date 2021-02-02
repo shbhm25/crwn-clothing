c9592aed9c7c7624393e5da51330f3d19350818a
Shubham Shekhar <shubham.shekhar@microfocus.com>
2021-02-02 23:03:05 +0530

    broken out homepage into smaller components,finished styling out homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import Reacr from 'react';
+import Directory from '../../components/directory/directory.component';
+import './homepage.styles.scss';
+
+
+const HomePage =() =>(
+    <div className='homepage'>
+        <Directory/>
+    </div>
+);
+
+export default HomePage;
